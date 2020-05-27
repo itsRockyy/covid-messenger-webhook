6d13ef0a1665128b7b191bdf25267c946b5ce29d
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-05-28 01:20:39 +0530

    fix(app): Remove reading text for all messages

--- index.js
+++ index.js
@@ -36,11 +36,6 @@ app.post("/webhook", (req, res) => {
       const webhook_event = entry.messaging[0];
       const sender_psid = webhook_event.sender.id;
 
-      console.log(webhook_event.message.text);
-      console.log("Sender PSID: " + sender_psid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
